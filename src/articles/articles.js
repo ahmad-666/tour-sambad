da815a3d74d48191a4cb89b3d086bee9d52abca8
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-12-18 13:34:13 -0800

    After first approve

---
+++ src/articles/articles.js
@@ -0,0 +1,10 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+// import '../utilities/styles/form.scss'
+import '../init.scss' ;
+import '../index/styles/header.scss' ;
+import '../index/styles/footer.scss' ;
+//load scripts
+import '../index/scripts/nav.js' ;
+//load videos
