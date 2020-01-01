8c7e197e2bda14b6c538f44ef1051a1e541132b7
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-01 14:15:03 -0800

    full resp

--- src/agency/scripts/agency.js
+++ src/agency/scripts/agency.js
@@ -4,12 +4,12 @@ import Collapse from '../../utilities/scripts/collapse.js' ;
 let FadeEffect = util.FadeEffect ;
 let SwapIconText = util.SwapIconText ;
 let agencyWrapper = document.querySelector('#agency') ;
-agencyWrapper.querySelectorAll('.withCollapse').forEach((withCollapse,i,all) => {
-   all = [...all] ;
-   let others = all.filter(elm =>elm!=withCollapse);
-   let font = new FontFaceObserver('iranSans');
-   font.load().then(()=>new Collapse(withCollapse,others,'15em'));
-});
+// agencyWrapper.querySelectorAll('.withCollapse').forEach((withCollapse,i,all) => {
+//    all = [...all] ;
+//    let others = all.filter(elm =>elm!=withCollapse);
+//    let font = new FontFaceObserver('iranSans');
+//    font.load().then(()=>new Collapse(withCollapse,others,'15em'));
+// });
 agencyWrapper.querySelectorAll('.fadeEffect').forEach(fadeEffect=>new FadeEffect(fadeEffect,'75%','white'));
 let swapInstances = [] ;
 agencyWrapper.querySelectorAll('.swapIconText').forEach(swap=>swapInstances.push(new SwapIconText(swap,'نمایش بیشتر','نمایش کمتر',['fas','fa-plus'],['fas','fa-minus'],swap.parentElement.querySelector('.fade'))));
